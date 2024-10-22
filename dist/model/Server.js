f038b7239a12317830f2887db1d5b2a68246a33b
sebastianAcunaZionit <sacuna@zionit.cl>
2024-10-22 16:33:09 -0300

    fix puerto

--- dist/model/Server.js
+++ dist/model/Server.js
@@ -11,25 +11,25 @@ const clima_1 = require("../router/v1/clima");
 class Server {
     constructor() {
         this.app = (0, express_1.default)();
-        this.port = Number(process.env.PORT) | 3000;
+        this.port = Number(process.env.PORT) || 3000;
         this.middlewares();
         this.routes();
     }
     middlewares() {
         this.app.use((0, cors_1.default)());
         this.app.use(express_1.default.json());
-        this.app.use(express_1.default.static('public'));
-        this.app.use((0, express_fileupload_1.default)({ useTempFiles: true, tempFileDir: '/tmp/' }));
+        this.app.use(express_1.default.static("public"));
+        this.app.use((0, express_fileupload_1.default)({ useTempFiles: true, tempFileDir: "/tmp/" }));
     }
     routes() {
-        this.app.use('/api/v1/auth', router_1.authRouter);
-        this.app.use('/api/v1/filters', router_1.filterRouter);
-        this.app.use('/api/v1/home', router_1.homeRouter);
-        this.app.use('/api/v1/resumen', router_1.resumenRouter);
-        this.app.use('/api/v1/visitas', router_1.visitasRouter);
-        this.app.use('/api/v1/ingresos', router_1.ingresosRouter);
-        this.app.use('/api/v1/libro-campo', router_1.libroCampoRouter);
-        this.app.use('/api/v1/clima', clima_1.climaRouter);
+        this.app.use("/api/v1/auth", router_1.authRouter);
+        this.app.use("/api/v1/filters", router_1.filterRouter);
+        this.app.use("/api/v1/home", router_1.homeRouter);
+        this.app.use("/api/v1/resumen", router_1.resumenRouter);
+        this.app.use("/api/v1/visitas", router_1.visitasRouter);
+        this.app.use("/api/v1/ingresos", router_1.ingresosRouter);
+        this.app.use("/api/v1/libro-campo", router_1.libroCampoRouter);
+        this.app.use("/api/v1/clima", clima_1.climaRouter);
         // this.app.use('/api/dte', dteRouter);
     }
     listen() {
--- model/Server.ts
+++ model/Server.ts
@@ -1,53 +1,52 @@
-import express, {Application} from 'express';
-import cors from 'cors'
-import fileUpload from 'express-fileupload';
+import express, { Application } from "express";
+import cors from "cors";
+import fileUpload from "express-fileupload";
 
-import { authRouter, filterRouter, homeRouter, ingresosRouter, resumenRouter, visitasRouter, libroCampoRouter } from '../router/';
-import { climaRouter } from '../router/v1/clima';
-
-
+import {
+  authRouter,
+  filterRouter,
+  homeRouter,
+  ingresosRouter,
+  resumenRouter,
+  visitasRouter,
+  libroCampoRouter,
+} from "../router/";
+import { climaRouter } from "../router/v1/clima";
 
 export default class Server {
+  private app: Application;
+  private port: number;
 
-    private app: Application;
-    private port: number;
+  constructor() {
+    this.app = express();
+    this.port = Number(process.env.PORT) || 3000;
 
+    this.middlewares();
+    this.routes();
+  }
 
-    constructor(){
-        this.app = express();
-        this.port = Number(process.env.PORT) | 3000;
+  middlewares(): void {
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.static("public"));
+    this.app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }));
+  }
 
-        this.middlewares();
-        this.routes();
-    }
+  routes(): void {
+    this.app.use("/api/v1/auth", authRouter);
+    this.app.use("/api/v1/filters", filterRouter);
+    this.app.use("/api/v1/home", homeRouter);
+    this.app.use("/api/v1/resumen", resumenRouter);
+    this.app.use("/api/v1/visitas", visitasRouter);
+    this.app.use("/api/v1/ingresos", ingresosRouter);
+    this.app.use("/api/v1/libro-campo", libroCampoRouter);
+    this.app.use("/api/v1/clima", climaRouter);
+    // this.app.use('/api/dte', dteRouter);
+  }
 
-
-    middlewares():void { 
-
-        this.app.use( cors() );
-        this.app.use( express.json() );
-        this.app.use( express.static('public'));
-        this.app.use( fileUpload({useTempFiles:true, tempFileDir:'/tmp/'}) )
-
-    }
-
-
-    routes():void{
-        this.app.use('/api/v1/auth', authRouter);
-        this.app.use('/api/v1/filters', filterRouter);
-        this.app.use('/api/v1/home', homeRouter);
-        this.app.use('/api/v1/resumen', resumenRouter);
-        this.app.use('/api/v1/visitas', visitasRouter);
-        this.app.use('/api/v1/ingresos', ingresosRouter);
-        this.app.use('/api/v1/libro-campo', libroCampoRouter);
-        this.app.use('/api/v1/clima', climaRouter);
-        // this.app.use('/api/dte', dteRouter);
-    }
-
-
-    listen():void{
-        this.app.listen( this.port, () => {
-            console.log(`Server running on port : ${ this.port }`);
-        })
-    }
+  listen(): void {
+    this.app.listen(this.port, () => {
+      console.log(`Server running on port : ${this.port}`);
+    });
+  }
 }
